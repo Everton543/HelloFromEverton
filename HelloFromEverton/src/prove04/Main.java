02e032dc6b7bbf12727cb2dd049721c5b80ea663
GeorgeSpirit94 <72421946+GeorgeSpirit94@users.noreply.github.com>
2020-10-10 08:21:11 -0500

    Update Main.java

--- HelloFromEverton/src/prove04/Main.java
+++ HelloFromEverton/src/prove04/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
        System.out.println("Hello, my name is Everton Alves");
-       System.out.println("Hello, my name is Jorge Espiritu");
+       System.out.println("Hello Everton, my name is Jorge Espiritu");
     
     }
 }
